276099240b328112ca4343c02163702575196a67
Mathis Telschow <mathistelschow@gmail.com>
2023-04-02 21:50:02 +0200

    Remove style from component

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,17 +1,23 @@
-import React from "react";
+import React from 'react';
 import './FaceRecognition.css';
 
 const FaceRecognition = ({ imgUrl, box }) => {
-    return (
-        <div className="center ma">
-            <div className="absolute mt2"> 
-                <img alt='' id="inputimage" src={imgUrl} width='500px' height='auto'/>
-                <div className="bounding-box" style={{
-                    top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol
-                }}></div>
-            </div>
-        </div>
-    );
-}
+	return (
+		<div className='center ma'>
+			<div className='absolute mt2'>
+				<img alt='' id='inputimage' src={imgUrl} />
+				<div
+					className='bounding-box'
+					style={{
+						top: box.topRow,
+						right: box.rightCol,
+						bottom: box.bottomRow,
+						left: box.leftCol,
+					}}
+				></div>
+			</div>
+		</div>
+	);
+};
 
 export default FaceRecognition;
