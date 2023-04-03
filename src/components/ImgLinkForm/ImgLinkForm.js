1ac728d8bb44f2fe60eae56df3cb783fe2a658d2
Mathis Telschow <mathistelschow@gmail.com>
2023-04-03 22:25:41 +0200

    Change indentetions

--- src/components/ImgLinkForm/ImgLinkForm.js
+++ src/components/ImgLinkForm/ImgLinkForm.js
@@ -1,18 +1,27 @@
-import React from "react";
-import './ImgLinkForm.css'
+import React from 'react';
+import './ImgLinkForm.css';
 
 const ImgLinkForm = ({ onInputChange, onButtonSubmit }) => {
-    return (
-        <div>
-            <p className="f3 white ">
-                {'This Smart Brain will detect faces in your pictures. Give it a try'}
-            </p>
-            <div className="pa3 br3 shadow-5 center form">
-                <input className="f4 pa2 w-70 center" type='text'  onInput={onInputChange}/>
-                <button className="w-30 grow f4 lnk ph3 pv2 dib black bg-green" onClick={onButtonSubmit}>Detect</button>
-            </div>
-        </div>
-    );
+	return (
+		<div>
+			<p className='f3 white '>
+				{'This Smart Brain will detect faces in your pictures. Give it a try'}
+			</p>
+			<div className='pa3 br3 shadow-5 center form'>
+				<input
+					className='f4 pa2 w-70 center'
+					type='text'
+					onInput={onInputChange}
+				/>
+				<button
+					className='w-30 grow f4 lnk ph3 pv2 dib black bg-green'
+					onClick={onButtonSubmit}
+				>
+					Detect
+				</button>
+			</div>
+		</div>
+	);
 };
 
 export default ImgLinkForm;
