f4e20a8a34b293fe75eb3607cd23cace72ace68f
Mathis Telschow <mathistelschow@gmail.com>
2023-04-06 07:45:41 +0200

    Rename Component

--- src/components/BoundingBoxes/BoundingBoxes.js
+++ src/components/BoundingBoxes/BoundingBoxes.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './BoundingBoxes.css'
 
 const BoundingBoxes = ({ boxes }) => (
 	<div>
--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -1,21 +1,12 @@
 import React from 'react';
 import './FaceRecognition.css';
-import BoundingBoxes from '../Bounding-Boxes/Bounding-Box';
+import BoundingBoxes from '../BoundingBoxes/BoundingBoxes';
 
 const FaceRecognition = ({ imgUrl, box }) => {
 	return (
 		<div className='center ma'>
 			<div className='absolute mt2'>
 				<img alt='' id='inputimage' src={imgUrl} />
-				{/* <div
-					className='bounding-box'
-					style={{
-						top: box.topRow,
-						right: box.rightCol,
-						bottom: box.bottomRow,
-						left: box.leftCol,
-					}}
-				></div> */}
 				<BoundingBoxes boxes={box}/>
 
 			</div>
