ecaedb3d8c2907937a3ccb867f8a310a284d8738
Mathis Telschow <mathistelschow@gmail.com>
2023-04-28 19:04:45 +0200

    Change input placeholder

--- src/components/ImgLinkForm/ImgLinkForm.js
+++ src/components/ImgLinkForm/ImgLinkForm.js
@@ -5,11 +5,12 @@ const ImgLinkForm = ({ onInputChange, onButtonSubmit }) => {
 	return (
 		<div>
 			<p className='f3 white '>
-				{'This Smart Brain will detect faces in your pictures. Give it a try'}
+				{'This Smart Brain will detect faces in your pictures. Give it a try!'}
 			</p>
 			<div className='pa3 br3 shadow-5 center form'>
 				<input
 					className='f4 pa2 w-70 center'
+					placeholder='Put an Image-URL here'
 					type='text'
 					onInput={onInputChange}
 				/>
